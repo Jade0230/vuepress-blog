ff6b4de74b38d619e26c9b53e52ac6a4b8ada421
renjie <renjie0229@outlook.com>
2023-04-24 16:03:26 +0800

    搭建vuepress项目

---
+++ .vuepress/config/navConf.js
@@ -0,0 +1,33 @@
+module.exports = [
+  {
+    "text": "首页",
+    "link": "/",
+    "icon": "reco-home"
+  },
+  {
+    "text": "文档",
+    "icon": "reco-document",
+    "items": [
+      {
+        "text": "JS高级",
+        "link": "/docs/JS_Advanced/"
+      }
+    ]
+  },
+  // {
+  //   "text": "时间线",
+  //   "link": "/timeline/",
+  //   "icon": "reco-date"
+  // },
+  // {
+  //   "text": "联系我",
+  //   "icon": "reco-message",
+  //   "items": [
+  //     {
+  //       "text": "GitHub",
+  //       "link": "https://github.com/recoluan",
+  //       "icon": "reco-github"
+  //     }
+  //   ]
+  // }
+]
---
+++ README.md
@@ -0,0 +1,17 @@
+---
+home: true
+heroText: null
+tagline: null
+# heroImage: /logo.png
+# heroImageStyle: {
+#   maxWidth: '600px',
+#   width: '100%',
+#   display: block,
+#   margin: '9rem auto 2rem',
+#   background: '#fff',
+#   borderRadius: '1rem',
+# }
+bgImageStyle: {
+  height: 0
+}
+---
